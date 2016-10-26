180bd7c1925a22d791ba119aec8556290f922cf0
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-10-26 00:12:33 +0000

    fixed date format

--- lookup_people.js
+++ lookup_people.js
@@ -1,7 +1,6 @@
 const pg = require ('pg');
 const settings = require ('./settings');
 const myArg = process.argv.slice(2).toString();
-console.log(typeof myArg);
 
 const client = new pg.Client({
   user     : settings.user,
@@ -16,7 +15,6 @@ client.connect ((err) => {
   if (err) {
     return (console.log(err));
   }
-  console.log("Searching...");
   client.query("SELECT id, first_name, last_name, birthdate FROM famous_people WHERE first_name LIKE $1 OR last_name LIKE $1", ["%"+myArg+"%"],
   (err, result) => {
     if (err) {
@@ -29,10 +27,13 @@ client.connect ((err) => {
 });
 
 function printResults(result, myArg){
-  console.log(typeof result);
-  console.log(`Found ${result.rows.length} person(s) by the query ${myArg}:`);
+  console.log(result.rows[0].birthdate);
+  let birthdateFormat = (result.rows[0].birthdate);
+  birthdateFormat =  birthdateFormat.toDateString();
+  console.log("Searching...");
+  console.log(`Found ${result.rows.length} person(s) by the query \'${myArg}\':`);
   result.rows.forEach(function (element){
-    console.log(`-1: ${element.first_name} ${element.last_name}, born \'${element.birthdate}\'`);
+    console.log(`-1: ${element.first_name} ${element.last_name}, born \'${birthdateFormat}\'`);
   });
 }
 
